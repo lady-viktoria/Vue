9fd1989decb34e07c75e247560373c2aebd07495
lady-viktoria <lady.vikusha@gmail.com>
2021-07-06 12:57:16 +0300

    lesson1

---
+++ lesson1/first-project/src/components/Calc.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div>
+      <input type="number" placeholder="op1" v-model.number="operand1" />
+      <input placeholder="op2" v-model.number="operand2" type="number" />
+      ={{ sum }}
+    </div>
+
+    <div>
+      <button @click="sum = operand1 + operand2">+</button>
+      <button @click="sum = operand1 - operand2">-</button>
+      <button v-on:click="sum = operand1 * operand2">*</button>
+      <button @click="div">/</button>
+      <button @click="sum = Math.trunc(operand1 / operand2)">Int/</button>
+      <button @click="degree">**</button>
+      <button @click="eventFn">Event</button>
+      result : {{ sum }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calc",
+  data: () => ({
+    operand1: 0,
+    operand2: 0,
+    sum: 0,
+  }),
+  methods: {
+    eventFn() {
+      console.log(arguments);
+    },
+    div() {
+      this.sum = this.operand1 / this.operand2;
+    },
+    degree() {
+      this.sum = Math.pow(this.operand1, this.operand2);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ lesson1/first-project/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Calc />
+    <HelloWorld 
+      msg="Welcome to Your Vue.js App" 
+      :myArr="arr"/>
+    Text: {{ message }} длина {{ message.length } }
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Calc from "@/components/Calc.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Calc
+  },
+  data: () => ({
+    message: "Ваша реклама",
+    arr: [1,2,3,4,5]
+  }),
+};
+</script>
